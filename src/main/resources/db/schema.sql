a76314e0416d11776af2547c38c34d605daf89cd
Teacher Yu <cn.yudequan@gmail.com>
2019-07-09 16:27:43 +0800

    20190708

---
+++ src/main/resources/db/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE emp (
+  emp_no int(11) PRIMARY KEY,
+  emp_name varchar(10)  NULL,
+  job varchar(9)  NULL,
+  mgr int(11)  NULL,
+  hire_date date  NULL,
+  sal double  NULL,
+  com double  NULL,
+  dept_no int(11)  NULL
+);
