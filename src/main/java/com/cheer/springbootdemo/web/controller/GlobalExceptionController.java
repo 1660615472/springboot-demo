879b129fba4ebc8a46b6c2aec695692f48b99a2a
Teacher Yu <cn.yudequan@gmail.com>
2019-07-10 09:24:27 +0800

    20190708

---
+++ src/main/java/com/cheer/springbootdemo/web/controller/GlobalExceptionController.java
@@ -0,0 +1,51 @@
+package com.cheer.springbootdemo.web.controller;
+
+import org.springframework.beans.ConversionNotSupportedException;
+import org.springframework.beans.TypeMismatchException;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.HttpMessageNotWritableException;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindException;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.ServletRequestBindingException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+// 全局异常处理
+@ControllerAdvice
+public class GlobalExceptionController {
+
+    // 异常默认处理
+    @ExceptionHandler
+    public String handleException(Exception e, Model model) {
+        model.addAttribute("message", e.getMessage());
+        model.addAttribute("cause", e.getCause());
+        return "error/error";
+    }
+
+    @ExceptionHandler({
+            HttpRequestMethodNotSupportedException.class,
+            HttpMediaTypeNotSupportedException.class,
+            HttpMediaTypeNotAcceptableException.class,
+            MissingServletRequestParameterException.class,
+            ServletRequestBindingException.class,
+            ConversionNotSupportedException.class,
+            TypeMismatchException.class,
+            HttpMessageNotReadableException.class,
+            HttpMessageNotWritableException.class,
+            MethodArgumentNotValidException.class,
+            MissingServletRequestPartException.class,
+            BindException.class,
+            NoHandlerFoundException.class})
+    public String handlePageNotFoundException(Exception e, Model model) {
+        model.addAttribute("message", e.getMessage());
+        model.addAttribute("cause", e.getCause());
+        return "error/404";
+    }
+}
---
+++ src/main/java/com/cheer/springbootdemo/web/interceptor/LoginCheckInterceptor.java
@@ -0,0 +1,29 @@
+package com.cheer.springbootdemo.web.interceptor;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Log4j2
+@Component
+public class LoginCheckInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.traceEntry();
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
